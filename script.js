53b0dfd63c94ac931ab8fac8c1924eb93fac90fe
Gardabramudyasuryadarma <125332329+Gardabramudyasuryadarma@users.noreply.github.com>
2025-05-02 21:07:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 function createHeart() {
-    const heart = document.createElement("div");
-    heart.classList.add("heart");
-    heart.style.left = Math.random() * window.innerWidth + "px";
-    heart.style.top = Math.random() * window.innerHeight + "px";
-    document.body.appendChild(heart);
-  
-    setTimeout(() => {
-      heart.remove();
-    }, 3000);
-  }
-  
-  setInterval(createHeart, 300);
-  
+  const heart = document.createElement("div");
+  heart.classList.add("heart");
+  heart.style.left = Math.random() * window.innerWidth + "px";
+  heart.style.top = Math.random() * window.innerHeight + "px";
+  document.body.appendChild(heart);
+
+  setTimeout(() => {
+    heart.remove();
+  }, 3000);
+}
+
+setInterval(createHeart, 300);
+
+function sendWA() {
+  const phoneNumber = "6281230759880"; // Ganti dengan nomor tujuan
+  const message = encodeURIComponent("IYA SAYANG");
+  const url = `https://wa.me/${phoneNumber}?text=${message}`;
+  window.open(url, "_blank");
+}
